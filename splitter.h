2643a4c59dd773cd1a5009fbd5e68dec2d539b93
Mark Wiering <markwiering@gmail.com>
2019-04-11 19:17:37 +0200

    Updated splitter.h

--- splitter.h
+++ splitter.h
@@ -1,12 +1,12 @@
 /* This header depends on the headers "vector.h" and <std::string>. */
 
-Vector<std::string> split(const std::string &code)
+Vector<std::string> split(const std::string &code, char a = ' ', char b = '\t')
 {
 	Vector<std::string> v; std::string t = "";
 	for(unsigned short int f = 1; f <= code.size(); f++)
 	{
-		if(code[f - 1] != ' ' && code[f - 1] != '\t') { t += code[f - 1]; }
-		if((code[f] == ' ' || code[f] == '\t') && (code[f - 1] != ' ' && code[f - 1] != '\t')) { v.add(t); t = "";}
+		if(code[f - 1] != a && code[f - 1] != b) { t += code[f - 1]; }
+		if((code[f] == a || code[f] == b) && (code[f - 1] != a && code[f - 1] != b)) { v.add(t); t = "";}
 	}
 	if(t != "" && t != "\t") { v.add(t); }
 	return v;

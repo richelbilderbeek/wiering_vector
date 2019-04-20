ae6ac0a941b8f86013308c832792bc1980a9155c
richelbilderbeek <richel@richelbilderbeek.nl>
2019-04-20 11:32:53 +0200

    Add asserts

--- splitter.h
+++ splitter.h
@@ -1,11 +1,17 @@
 /* This header depends on the headers "vector.h" and <std::string>. */
 
+#include <cassert>
+
 Vector<std::string> split(const std::string &code, char a = ' ', char b = '\t')
 {
 	Vector<std::string> v; std::string t = "";
 	for(unsigned short int f = 1; f <= code.size(); f++)
 	{
+    assert(f - 1 >= 0);
+    assert(f - 1 < code.size());
 		if(code[f - 1] != a && code[f - 1] != b) { t += code[f - 1]; }
+    assert(f >= 0);
+    assert(f < code.size());
 		if((code[f] == a || code[f] == b) && (code[f - 1] != a && code[f - 1] != b)) { v.add(t); t = "";}
 	}
 	if(t != "" && t != "\t") { v.add(t); }
